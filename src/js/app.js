fc755ccebdcc01ec6c1952aee18287f7a685b75b
MForever78 <shunjian1128@gmail.com>
2015-05-11 23:23:39 +0800

    Change to webpack standard structrue

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,30 @@
 /**
  * Created by MForever78 on 15/5/10.
  */
+
+var indexView = require("view/index");
+var sessionModel = require("model/session");
+
+var router = {
+  routing: function() {
+    page("/", this.index);
+    page("/user", this.user);
+    page("/login", this.login);
+    page("*", this.notFound);
+    page.start();
+  },
+
+  index: function() {
+
+  },
+
+  user: function() {
+
+  },
+
+  login: function() {
+
+  }
+};
+
+router.routing();
