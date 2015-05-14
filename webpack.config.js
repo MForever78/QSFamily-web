08e14ab115e8fa072d0b69aa2af0d1b6d0477162
MForever78 <shunjian1128@gmail.com>
2015-05-14 17:40:59 +0800

    Add favicon

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: "./build",
+    publicPath: "/build/",
+    filename: "app.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.less$/, loader: "style!css!less" },
+      { test: /\.html$/, loader: "html" }
+    ]
+  }
+}
