3ee62e37d578e1b175e75aea7187334962cf5ab6
MForever78 <MForever78@MForever78.local>
2015-06-07 19:32:08 +0800

    Session model complete

---
+++ src/js/models/session.js
@@ -0,0 +1,78 @@
+/**
+ * Created by MForever78 on 15/6/6.
+ */
+
+var createHash = require('sha.js');
+
+var SessionModel = Backbone.Model.extend({
+  ajax: function(url, opt) {
+    var self = this;
+    return new Promise(function(resolve, reject) {
+      _.extend(opt, {
+        success: function(data) {
+          if (data.code !== 200) {
+            // TODO: display in error view
+            console.log(data);
+            if (data.code === 404 || data.code === 403) {
+              self.logout();
+              window.location.href = '/';
+            }
+          } else {
+            resolve(data);
+          }
+        },
+        error: function(data) {
+          reject(data);
+        },
+        cache: false,
+        dataType: 'jsonp',
+        contentType: 'application/json'
+      });
+      $.ajax(self.baseURL + url, opt);
+    });
+  },
+
+  initialize: function(opt) {
+    this.baseURL = opt.baseURL;
+    this.token = localStorage.getItem("QSFamily-token");
+    this.userid = localStorage.getItem("QSFamily-user-id");
+  },
+
+  loggedIn: function() {
+    return this.token && this.userid;
+  },
+
+  login: function(username, password) {
+    var self = this;
+    return new Promise(function(resolve, reject) {
+      var sha256 = createHash('sha256');
+      self.ajax("login", {
+        data: {
+          role: 'teacher',
+          username: username,
+          password: sha256.update(self.salt).update(password).digest('hex')
+        },
+        method: 'POST'
+      }).then(function(result) {
+        self.token = result.token;
+        self.userid = reqsult.userid;
+        self.saveStorage();
+        resolve(null);
+      }).catch(function(err) {
+        reject(err);
+      });
+    });
+  },
+
+  logout: function() {
+    localStorage.removeItem('QSFamily-token');
+    localStorage.removeItem('QSFamily-user-id');
+  },
+
+  saveStorage: function() {
+    localStorage.setItem('QSFamily-token', this.token);
+    localStorage.setItem('QSFamily-user-id', this.userid);
+  }
+});
+
+module.exports = SessionModel;
