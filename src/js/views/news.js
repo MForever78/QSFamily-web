d483354e92a3ac3ec69b6761448894aaea486ae9
MForever78 <shunjian1128@gmail.com>
2015-09-06 21:18:11 +0800

    Change news view to fit CRUD standard

--- src/js/views/news.js
+++ src/js/views/news.js
@@ -8,9 +8,8 @@ var NavView = require("./nav");
 var NewsView = Backbone.View.extend({
   initialize: function(opt) {
     this.sessionModel = opt.sessionModel;
-    this.newsModel = opt.newsModel;
-    this.newsid = opt.newsid;
     this.router = opt.router;
+    this.newsModel = opt.newsModel;
     this.render();
   },
 
@@ -28,38 +27,35 @@ var NewsView = Backbone.View.extend({
     });
     var template = _.template($("#news-template").html());
     var self = this;
-    this.newsModel.getNewsById(this.newsid)
-      .then(function(data) {
-        data.news.date = new Date(data.news.create_at);
-        var editable = self.sessionModel.profile.role === 'teacher';
-        self.$el.html(template({
-          editable: editable,
-          news: data.news
-        })).hide().fadeIn(300);
-      });
+    this.newsModel.fetch({success: function(news) {
+      var editable = self.sessionModel.profile.role === 'teacher';
+      self.$el.html(template({
+        editable: editable,
+        news: news
+      })).hide().fadeIn(300);
+    }});
   },
 
   editNews: function(event) {
     event.preventDefault();
-    this.router.navigate("edit/" + this.newsid, { trigger: true });
+    this.router.navigate("edit/" + this.newsModel.id, { trigger: true });
   },
 
   deleteNews: function(event) {
     event.preventDefault();
     var self = this;
-    this.newsModel.deleteNews(this.newsid)
-      .then(function() {
-        var messageView = MessageView.instance || new MessageView;
-        messageView.display({
-          type: 'success',
-          message: "删除成功",
-          parent: $("#post-wrap"),
-          icon: "checkmark"
-        });
-        setTimeout(function() {
-          self.router.navigate("", { trigger: true });
-        }, 600);
+    this.newsModel.destroy({success: function() {
+      var messageView = MessageView.instance || new MessageView;
+      messageView.display({
+        type: 'success',
+        message: "删除成功",
+        parent: $("#post-wrap"),
+        icon: "checkmark"
       });
+      setTimeout(function() {
+        self.router.navigate("", { trigger: true });
+      }, 600);
+    }});
   }
 });
 
